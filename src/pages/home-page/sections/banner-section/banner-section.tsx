3345356f43cc1ac272225dc638042c2efbc45a3b
jsswpt <paiusov.d@yandex.ru>
2023-01-02 20:19:38 +0500

    немного стилей

--- src/pages/home-page/sections/banner-section/banner-section.tsx
+++ src/pages/home-page/sections/banner-section/banner-section.tsx
@@ -4,6 +4,7 @@ import st from "../../styles.module.scss";
 import cn from "classnames";
 import { Container, Plate } from "shared/ui";
 import Mars from "shared/assets/imgs/mars.png";
+import Stars from "shared/assets/imgs/custom-stars.png";
 
 export const BannerSection = () => {
   return (
@@ -15,15 +16,28 @@ export const BannerSection = () => {
               Путешествие <span>на красную планету</span>
             </h1>
           </div>
-          <button>Начать путешествие</button>
+          <button className={st.button}>Начать путешествие</button>
         </div>
+        <div className={st.stars_wrap}>
+          <img src={Stars} alt="stars" />
+        </div>
+        <div className={st.sun_wrap}></div>
         <div className={st.mars_wrap}>
-          <img src={Mars} alt="" />
+          <img src={Mars} alt="mars" />
         </div>
         <div className={cn(st.banner_wrap, st.banner_wrap__achievements)}>
-          <Plate>123</Plate>
-          <Plate>123</Plate>
-          <Plate>123</Plate>
+          <div className={st.grid_item}>
+            <Plate>123</Plate>
+          </div>
+          <div className={st.grid_item}>
+            <Plate>123</Plate>
+          </div>
+          <div className={st.grid_item}>
+            <Plate>123</Plate>
+          </div>
+          <div className={st.grid_item}>
+            <Plate>123</Plate>
+          </div>
         </div>
       </Container>
     </section>
